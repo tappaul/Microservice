689e040afeeb09a7a0c6bf6aab407767ce3d79e3
Tapas Paul <paul_t@India.XoriantCorp.com>
2017-10-30 15:00:01 +0530

    Initial Commit

---
+++ src/main/java/com/example/Application/configaration/ApplicationConfig.java
@@ -0,0 +1,26 @@
+package com.example.Application.configaration;
+
+import javax.inject.Named;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationConfig {
+
+	@Named
+	static class JerseyConfig extends ResourceConfig {
+		public JerseyConfig() {
+			this.packages("com.example.Application.common.rest");
+		}
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate;
+	}
+
+}
